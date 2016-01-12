09c512bf4960c995aa58e251da476a87adb8e996
Natalie Chow <cychownat@gmail.com>
2016-01-12 12:16:34 -0800

    Added test for tcp server logging

--- natalie_chow/test/test.js
+++ natalie_chow/test/test.js
@@ -1,5 +1,8 @@
 const expect = require('chai').expect;
+const net = require('net');
+const fs = require('fs');
 const identifier = require(__dirname + '/../lib/identifier');
+const server = require(__dirname + '/../lib/server');
 
 describe('Identifier generate function', function() {
   it('should return a filename with .txt extension', function() {
@@ -16,5 +19,43 @@ describe('Identifier generate function', function() {
       expect(filename1).to.not.eql(filename2);
       done();
     }, 1500);
+  });
+});
+
+describe('TCP server', function() {
+  var requestString = 'Hello World\r\n';
+
+  before(function() {
+    server.start('/log_test.txt');
+    var client = net.createConnection(3000, function() {
+      console.log('Connected to server');
+      client.write(requestString);
+    });
+  });
+
+  after(function() {
+    fs.unlink(__dirname + '/../log_test.txt', function(err) {
+      if (err) console.log(err);
+    });
+  });
+
+  it('should write request to newly created text file', function(done) {
+    // no file before
+    fs.access(__dirname + '/../log_test.txt', fs.F_OK, function(err) {
+      expect(err).to.exist;
+    });
+
+    var readFile = function() {
+      fs.readFile(__dirname + '/../log_test.txt', function(err, data) {
+        if (err) {
+          expect(true).to.eql(false);
+          console.log(err);
+          return done();
+        }
+        expect(data.toString()).to.eql(requestString);
+        done();
+      });
+    };
+    setTimeout(readFile, 500);
   });
 });

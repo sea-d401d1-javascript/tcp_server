10ed7810abcba7bbeb1b5f69a04d982009f0e3b2
Yueyue07 <xww.qyy@gmail.com>
2016-01-12 00:02:50 -0800

    add timeStamp function int tcp_server.js

--- YueyueQin/tcp_server.js
+++ YueyueQin/tcp_server.js
@@ -1,20 +1,22 @@
 const net = require('net');
 const fs = require('fs');
+const time = require('./lib/timeStamp.js').timeStamp;
 
 var server = net.createServer(function(socket){
   console.log('client connected');
   socket.on('end',function() {
     console.log('client disconnected');
-    socket.end();
   });
   socket.pipe(socket);
-  socket.write('hello\r\n');
+  socket.write('Done\r\n');
 
-  var ws = fs.createWriteStream('test.txt');
+  var timeStamp = time();
+  var ws = fs.createWriteStream(__dirname + '/req_resp/' + timeStamp);
   socket.pipe(ws);
+  socket.end();
 
 });
 
 server.listen(2000,function(){
   console.log('server up');
-})
+});

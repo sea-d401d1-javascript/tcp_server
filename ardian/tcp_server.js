9f099d613861f58893653831fcb1a72aa7081f2c
Ardian Ajvazi <ardian_46@hotmail.com>
2016-01-11 22:33:19 -0800

    tcp_server assignment

---
+++ ardian/tcp_server.js
@@ -0,0 +1,17 @@
+const net = require('net');
+var fs = require('fs');
+
+var server = net.createServer(function(socket) {
+  var newFile = fs.createWriteStream('./logs/' + new Date() + '.txt');
+  socket.pipe(newFile);
+
+  console.log("received request");
+
+  socket.on('end', function() {
+  console.log('socket closed');
+  });
+});
+
+server.listen('3000', function() {
+  console.log('server up');
+});

587094a64bf0de85157db876a7fe7f421bf41a82
Gene Troy <genetroy@hotmail.com>
2016-02-19 12:04:57 -0800

    Genes first commit

--- YueyueQin/lib/timeStamp.js
+++
@@ -1,6 +0,0 @@
-exports.timeStamp = function() {
-  var date = new Date();
-  var timeStamp = date.getSeconds()+'_'+date.getMinutes()+'_'+date.getHours()+'_'+date.getDate();
-  //Secs_Mins_Hours_Days
-  return timeStamp;
-};
